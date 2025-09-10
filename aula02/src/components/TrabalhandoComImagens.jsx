43d60ad4971a126729931c4da8cb6cda96700979
Dyego Kelven <dkelvens@hotmail.com>
2025-09-09 23:55:59 -0300

    Trabalhando com imagens

---
+++ aula02/src/components/TrabalhandoComImagens.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Yamal from '../assets/yamal.webp'
+
+const TrabalhandoComImagens = () => {
+  return (
+    <div>
+        <img src="MESSI.webp"  alt="" />
+        <img src={Yamal}  alt=""/>
+    </div>
+  )
+}
+
+export default TrabalhandoComImagens
