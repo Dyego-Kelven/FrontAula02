13305edb07f8297693a03f5cb475e3c9a9ef41b0
Dyego Kelven <dkelvens@hotmail.com>
2025-10-01 22:48:30 -0300

    Criando Listas

--- aula03/src/App.jsx
+++ aula03/src/App.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
-import EstouConseguindoAprenderReact from './components/EstouConseguindoAprenderReact'
-import EstouComSono from './components/EstouComSono'
+import ListaDeLinguagens from './components/ListaDeLinguagens'
+
+
 
 const App = () => {
   return (
     <div>
-      <EstouComSono comSono={true}/>
+    {
+      [
+        {linguagem: "C++", dificuldade:"Difícil"},
+        {linguagem: "C#", dificuldade:"Difícil"},
+        {linguagem: "Python", dificuldade:"Difícil"},
+        {linguagem: "JavaScript", dificuldade:"Difícil"},
+        {linguagem: "Java", dificuldade:"IMPOSSÍVEL"}
+
+      ].map((linguagem) =>
+        <ListaDeLinguagens linguagem={linguagem.linguagem} dificuldade={linguagem.dificuldade}/>
+      )
+    } 
 
     </div>
   )
---
+++ aula03/src/components/ListaDeLinguagens.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ListaDeLinguagens = ({linguagem, dificuldade}) => {
+  return (
+    <div>
+        <p>Linguagem: {linguagem}</p>
+        <p>Dificuldade: {dificuldade}</p>
+    </div>
+  )
+}
+
+export default ListaDeLinguagens
+
+
+
